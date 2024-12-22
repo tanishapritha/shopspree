93a60b51635836b551310d1926884f0ca54eb24e
tanishapritha <tpritha1903@gmail.com>
2024-12-22 14:22:12 +0530

    basic

---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import GiShoppingBag from "react-icons/gi";
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Navbar content */}
+    </nav>
+  );
+};
+
+export default Navbar;
