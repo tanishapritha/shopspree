f987d5789b2447579d1d337dd0261b4314d6fca2
tanishapritha <tpritha1903@gmail.com>
2024-12-27 13:23:29 +0530

    Updated Navbar and App CSS to fix header.

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,22 +1,33 @@
 // filepath: /C:/Users/tprit/Documents/GitHub/shopspree/frontend/src/Components/Navbar/Navbar.js
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Navbar.css';
-
 import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navbarRef = useRef(null);
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClickOutside = (event) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div>
-    
-      <nav className={`navbar ${isOpen ? 'open' : ''}`}>  
-           
-      <FaBars className="menu-icon" onClick={toggleNavbar} />     
+      <FaBars className="menu-icon" onClick={toggleNavbar} />
+      <nav ref={navbarRef} className={`navbar ${isOpen ? 'open' : ''}`}>
         <ul className="navbar-menu">
           <li className="navbar-item"><a href="#appliances">APPLIANCES</a></li>
           <li className="navbar-item"><a href="#clothes">CLOTHES</a></li>
@@ -24,10 +35,6 @@ const Navbar = () => {
           <li className="navbar-item"><a href="#edibles">EDIBLES</a></li>
         </ul>
       </nav>
-      
-      
-        
-      
     </div>
   );
 };
